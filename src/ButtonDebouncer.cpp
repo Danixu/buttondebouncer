39c2ac5d6aaf1349e27b2315aeb2f8480ace86b1
Daniel Carrasco <danielmadrid19@gmail.com>
2020-11-17 22:50:30 +0100

    First working version

---
+++ src/ButtonDebouncer.cpp
@@ -0,0 +1,163 @@
+/*
+
+  Debounce buttons and trigger events
+  Copyright (C) 2020-2020 by Daniel Carrasco <admin at electrosoftcloud dot com>
+
+  Based on Debounce Event library: https://github.com/xoseperez/debounceevent
+
+  The ButtonDebouncer library is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  The ButtonDebouncer library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with the ButtonDebouncer library.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <Arduino.h>
+#include "ButtonDebouncer.h"
+
+
+ButtonDebouncer::ButtonDebouncer(uint8_t pin, DEBOUNCE_EVENT_CALLBACK_SIGNATURE, uint8_t mode, unsigned long delay, unsigned long repeat, unsigned long long_press_delay, unsigned long burst_delay, unsigned long burst_interval) {
+    this->callback = callback;
+    _init(pin, mode, delay, repeat, long_press_delay, burst_delay, burst_interval);
+}
+
+ButtonDebouncer::ButtonDebouncer(uint8_t pin, uint8_t mode, unsigned long delay, unsigned long repeat, unsigned long long_press_delay, unsigned long burst_delay, unsigned long burst_interval) {
+    this->callback = NULL;
+    _init(pin, mode, delay, repeat, long_press_delay, burst_delay, burst_interval);
+}
+
+
+void ButtonDebouncer::_init(uint8_t pin, uint8_t mode, unsigned long delay, unsigned long repeat, unsigned long long_press_delay, unsigned long burst_delay, unsigned long burst_interval) {
+
+    // store configuration
+    _pin = pin;
+    _mode = mode & 0x01;
+    _defaultStatus = ((mode & BUTTON_DEFAULT_HIGH) > 0);
+    _delay = delay;
+    _repeat = repeat;
+    _long_press_delay = long_press_delay;
+    _burst_delay = burst_delay;
+    _burst_interval = burst_interval;
+
+    // set up button
+    #if ESP8266
+    if (_pin == 16) {
+        if (_defaultStatus) {
+            pinMode(_pin, INPUT);
+        } else {
+            pinMode(_pin, INPUT_PULLDOWN_16);
+        }
+    } else {
+    #endif // ESP8266
+        if ((mode & BUTTON_SET_PULLUP) > 0) {
+            pinMode(_pin, INPUT_PULLUP);
+        } else {
+            pinMode(_pin, INPUT);
+        }
+    #if ESP8266
+    }
+    #endif // ESP8266
+
+    _status = (_mode == BUTTON_SWITCH) ? digitalRead(_pin) : _defaultStatus;
+    _last_status = _status;
+
+}
+
+unsigned char ButtonDebouncer::loop() {
+
+    unsigned char event = EVENT_NONE;
+
+    if (digitalRead(_pin) != _status) {
+        if (digitalRead(_pin) != _last_status)
+        {
+
+            _last_event = millis();
+            _last_status = digitalRead(_pin);
+
+        }
+        else if (millis() - _last_event > _delay)
+        {
+
+            _status = !_status;
+
+            if (_mode == BUTTON_SWITCH)
+            {
+
+                event = EVENT_CHANGED;
+
+            } else {
+
+                // released
+                if (_status == _defaultStatus)
+                {
+
+                    _event_length = millis() - _event_start;
+                    _ready = true;
+
+                // pressed
+                }
+                else
+                {
+
+                    event = EVENT_PRESSED;
+                    _event_start = millis();
+                    _event_length = 0;
+                    if (_reset_count)
+                    {
+                        _event_count = 1;
+                        _reset_count = false;
+                    }
+                    else
+                    {
+                        ++_event_count;
+                    }
+                    _ready = false;
+
+                }
+            }
+        }
+    }
+    else if (digitalRead(_pin) != _last_status && (millis() - _last_event > _delay))
+    {
+        _last_status = digitalRead(_pin);
+    }
+
+    if (_ready && (millis() - _event_start > _repeat)) {
+        _ready = false;
+        _reset_count = true;
+        if (!_long_press)
+        {
+            event = EVENT_RELEASED;
+        }
+        _long_press = false;
+        _burst_count = 0;        
+    }
+
+    if (!_ready && _status != _defaultStatus)
+    {
+        if ((millis() - _event_start > _long_press_delay) && !_long_press){
+            event = EVENT_LONG_PRESSED;
+            _long_press = true;
+        }
+        else if ((millis() - _event_start > _burst_delay) && (((millis() - _event_start) / _burst_interval) > _burst_count))
+        {
+            event = EVENT_BURST;
+            _burst_count = ((millis() - _event_start) / _burst_interval);
+        }
+    }
+
+    if (event != EVENT_NONE) {
+        if (this->callback) this->callback(_pin, event, _event_count, _event_length);
+    }
+
+    return event;
+
+}
---
+++ src/ButtonDebouncer.h
@@ -0,0 +1,86 @@
+/*
+
+  Debounce buttons and trigger events
+  Copyright (C) 2020-2020 by Daniel Carrasco <admin at electrosoftcloud dot com>
+
+  The ButtonDebouncer library is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  The ButtonDebouncer library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with the ButtonDebouncer library.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef _DEBOUNCE_EVENT_h
+#define _DEBOUNCE_EVENT_h
+
+#ifdef ESP8266
+#include <functional>
+#define DEBOUNCE_EVENT_CALLBACK_SIGNATURE std::function<void(uint8_t pin, uint8_t event, uint8_t count, uint16_t length)> callback
+#else
+#define DEBOUNCE_EVENT_CALLBACK_SIGNATURE void (*callback)(uint8_t pin, uint8_t event, uint8_t count, uint16_t length)
+#endif
+
+#define BUTTON_PUSHBUTTON       0
+#define BUTTON_SWITCH           1
+#define BUTTON_DEFAULT_HIGH     2
+#define BUTTON_SET_PULLUP       4
+
+#define DEBOUNCE_DELAY          50
+#define REPEAT_DELAY            500
+#define BURST_DELAY             1000
+#define BURST_INTERVAL          100
+#define LONG_PRESS_DELAY        1000
+
+#define EVENT_NONE              0
+#define EVENT_CHANGED           1
+#define EVENT_PRESSED           2
+#define EVENT_LONG_PRESSED      3
+#define EVENT_RELEASED          4
+#define EVENT_BURST             5
+
+class ButtonDebouncer {
+
+    public:
+
+        ButtonDebouncer(uint8_t pin, DEBOUNCE_EVENT_CALLBACK_SIGNATURE, uint8_t mode = BUTTON_PUSHBUTTON | BUTTON_DEFAULT_HIGH, unsigned long delay = DEBOUNCE_DELAY, unsigned long repeat = REPEAT_DELAY, unsigned long long_press_delay = LONG_PRESS_DELAY, unsigned long burst_delay = BURST_DELAY, unsigned long burst_interval = BURST_INTERVAL);
+        ButtonDebouncer(uint8_t pin, uint8_t mode = BUTTON_PUSHBUTTON | BUTTON_DEFAULT_HIGH, unsigned long delay = DEBOUNCE_DELAY, unsigned long repeat = REPEAT_DELAY, unsigned long long_press_delay = LONG_PRESS_DELAY, unsigned long burst_delay = BURST_DELAY, unsigned long burst_interval = BURST_INTERVAL);
+        unsigned char loop();
+        bool pressed() { return (_status != _defaultStatus); }
+        unsigned long getEventLength() { return _event_length; }
+        unsigned long getEventCount() { return _event_count; }
+
+    private:
+
+        uint8_t _pin;
+        uint8_t _mode;
+        bool _status;
+        bool _last_status;
+        bool _ready = false;
+        bool _reset_count = true;
+        bool _long_press = false;
+        unsigned long _event_start;
+        unsigned long _event_length;
+        unsigned char _event_count = 0;
+        unsigned char _burst_count = 0;
+        unsigned long _last_event;
+        uint8_t _defaultStatus;
+        unsigned long _delay;
+        unsigned long _long_press_delay;
+        unsigned long _burst_delay;
+        unsigned long _burst_interval;
+        unsigned long _repeat;
+        DEBOUNCE_EVENT_CALLBACK_SIGNATURE;
+
+        void _init(uint8_t pin, uint8_t mode, unsigned long delay, unsigned long repeat, unsigned long long_press_delay, unsigned long burst_delay, unsigned long burst_interval);
+
+};
+
+#endif
